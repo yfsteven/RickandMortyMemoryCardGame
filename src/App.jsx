bc69f86e5d9acb06fcad58460b95c0c3637ff2dc
yfsteven <yaninquiry@gmail.com>
2024-02-04 12:05:22 -0500

    Fix bug preventing API data to be mapped out

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
 import { useState, useEffect } from 'react'
 import Card from "./components/Card.jsx"
 import './App.css'
+import { getCharacter } from 'rickmortyapi'
 
 function App() {
-  const [count, setCount] = useState(0);
   const [characters, setCharacter] = useState([]);
 
 
   useEffect(() => {
-    const apiUrl = "https://rickandmortyapi.com/api/character";
-
     const fetchData = async () => {
       try {
-        const res = await fetch(apiUrl);
-        const data = await res.json();
-        setCharacter([data.results]);
+        const theSmiths = await getCharacter([1, 2, 3, 4, 5 ]);
+        setCharacter([theSmiths.data])
       } catch (error) {
         console.log("error", error);
       }
     };
     fetchData(); 
   }, []);
-  console.log(characters[0]);
+  console.log(characters[0])
   return (
     <div>
-      {characters[0].map(({id, image}) => {
+     {characters[0].map(({id, image}) => {
         return <Card key={id} photo={image} />
       })}
     </div>
