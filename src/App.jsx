5ebdf33766690aef811194ca142fd09e2a9e063a
yfsteven <yaninquiry@gmail.com>
2024-02-04 19:51:13 -0500

    Components were rendering without data so added ternary operator when there's none

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,51 @@ import { getCharacter } from 'rickmortyapi'
 function App() {
   const [characters, setCharacter] = useState([]);
 
+  const fetchData = async () => {
+    try {
+      const theSmiths = await getCharacter([1, 2, 3, 4, 5 ]);
+      const data =  await theSmiths.data
+      setCharacter([data])
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const theSmiths = await getCharacter([1, 2, 3, 4, 5 ]);
-        setCharacter([theSmiths.data])
-      } catch (error) {
-        console.log("error", error);
-      }
-    };
     fetchData(); 
   }, []);
   console.log(characters[0])
   return (
     <div>
-     {characters[0].map(({id, image}) => {
+     {characters.length > 0 ? characters[0].map(({id, image}) => {
         return <Card key={id} photo={image} />
-      })}
+      }) : <p>Loading</p>}
     </div>
   )
 }
+
+/*
+/*
+const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    async function getCharacters() {
+      const res = await fetch("https://rickandmortyapi.com/api/character");
+      const data = await res.json();
+
+      setCharacters(data.results);
+    }
+    getCharacters();
+  }, [])
+  console.log(characters);
+  return (
+    <div>
+      {characters.length > 0 ? characters.map(({id, image}) => {
+        return <Card key={id} photo={image}/>
+      }) : <div>Loading..</div>}
+    </div>
+  )
+*/
+*/
 
 export default App
